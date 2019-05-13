a44d14494618151c0133e3b08afd269e936d2a7f
pocketsystem <pocketsystem247@gmail.com>
2019-05-13 23:24:49 +0900

    微調整

--- coolsite.php
+++ coolsite.php
@@ -1,7 +1,7 @@
 <?php
 
 // Jsonファイル読込
-$file = __DIR__."/sitelist.json";
+$file = __DIR__ . "/sitelist.json";
 $json = file_get_contents($file);
 $obj  = json_decode($json, true);
 
@@ -19,35 +19,36 @@ foreach ($obj as $data) {
     }
 
     $out .= "<div class='siteitem'>";
-    $out .= "<a href='".$url."'>";
-    $out .= "<div>";
-    $out .= "<img src='img/".$img."'><br>".$snm;
-    $out .= "</div>";
-    $out .= "</a>";
+    $out .= "   <a href='" . $url . "'>";
+    $out .= "      <div>";
+    $out .= "       <img src='img/" . $img . "'><br>" . $snm;
+    $out .= "      </div>";
+    $out .= "   </a>";
     $out .= "</div>";
 }
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>COOL WEB SITE</title>
-	<link rel="stylesheet" href="css/style.css" type="text/css" />
-    <script	src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-    <script src="js/main.js" charset="utf-8"></script>
-</head>
-<body>
-	<h1>COOL WEB SITE</h1>
-	<div>We collected simple and cool sites</div>
 
-	<div>
-		<div class="sitelist">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/style.css" type="text/css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <title>COOL WEB SITE</title>
+</head>
+
+<body>
+    <h1>COOL WEB SITE</h1>
+    <div>We collected simple and cool sites</div>
+    <div>
+        <div class="sitelist">
             <?php
-                echo $out;
+            echo $out;
             ?>
         </div>
-	</div>
+    </div>
 </body>
+
 </html>
--- js/main.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-	if (navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/)) {
-		$('.qrcode').hide();
-	} else {
-		$('.qrcode').show();
-	}
-});
